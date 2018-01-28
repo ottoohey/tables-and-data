01d54c1021c138f96be3932da5ebc54004b6e751
Oliver Toohey <ottoohey@gmail.com>
2018-01-28 12:50:51 +1100

    third lesson done

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Oliver Toohey on 26/1/18.
+//  Copyright © 2018 Oliver Toohey. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+ 
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
